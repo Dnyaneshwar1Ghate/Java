dd6e2889068b80f1475a65c214f2bc3305c4accd
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2024-06-02 01:26:39 +0530

    java basic

---
+++ NestedIFDemo.java
@@ -0,0 +1,21 @@
+public class NestedIFDemo 
+{  
+    public static void main(String[] args)
+     {  
+        String city = "Agra";  
+        if (city == "Meerut") 
+        {
+            System.out.println("city is meerut");
+        }
+        else if (city == "Noida") 
+        {  
+            System.out.println("city is noida");  
+        } else if (city == "Agra") 
+        {  
+            System.out.println("city is agra");  
+        } else 
+        {  
+            System.out.println(city);  
+        }  
+    }  
+}  
